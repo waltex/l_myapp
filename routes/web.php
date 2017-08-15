aae3d0e7bb0f12066605a97d4a85ef53c686827b
Walter Cardelli (imac) <waltex@libero.it>
2017-08-15 23:01:37 +0200

    tutorial laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/hello',function(){
+    return 'Hello World!';
+});
+*/
+Route::get('/hello/{name}', 'Hello@show');
+//Route::get('hello', '/Hello@index');
